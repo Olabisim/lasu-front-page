62509346a26624075a0a9362187f32a833ecb320
Olabisi <ajoseholabisi@gmail.com>
2021-04-09 16:03:39 +0100

    fresh start

---
+++ js/s_slider.js
@@ -0,0 +1,11 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:2, 
+  // items change number for slider display on desktop
+  
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
